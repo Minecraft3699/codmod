73d63a1118776939c31c8cd21942ce860b767c7e
Minecraft3699 <dylandirkes256@outlook.com>
2025-05-13 08:48:03 -0500

    models and stuff

---
+++ src/main/java/com/mc3699/codmod/entity/yellowWisp/YellowWispModel.java
@@ -0,0 +1,66 @@
+package com.mc3699.codmod.entity.yellowWisp;// Made with Blockbench 4.12.2
+// Exported for Minecraft version 1.17 or later with Mojang mappings
+// Paste this class into your mod and generate all required imports
+
+
+import com.mc3699.codmod.Codmod;
+import com.mc3699.codmod.client.EntityAnimations;
+import com.mc3699.codmod.entity.swarmCod.SwarmCodEntity;
+import com.mc3699.codmod.entity.yellowWisp.YellowWispEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.animation.AnimationDefinition;
+import net.minecraft.client.animation.KeyframeAnimations;
+import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import org.joml.Vector3f;
+
+public class YellowWispModel extends HierarchicalModel<YellowWispEntity> {
+	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(Codmod.MODID, "yellow_wisp"), "main");
+	private final ModelPart body;
+	private final ModelPart base;
+	private final ModelPart outer;
+
+	public YellowWispModel(ModelPart root) {
+		this.body = root.getChild("body");
+		this.base = this.body.getChild("base");
+		this.outer = this.body.getChild("outer");
+	}
+
+	public static LayerDefinition createBodyLayer() {
+		MeshDefinition meshdefinition = new MeshDefinition();
+		PartDefinition partdefinition = meshdefinition.getRoot();
+
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+		PartDefinition base = body.addOrReplaceChild("base", CubeListBuilder.create().texOffs(0, 8).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -4.0F, 0.0F));
+
+		PartDefinition outer = body.addOrReplaceChild("outer", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -2.0F, -2.0F, 4.0F, 4.0F, 4.0F, new CubeDeformation(1.5F)), PartPose.offset(0.0F, -4.0F, 0.0F));
+
+		return LayerDefinition.create(meshdefinition, 16, 16);
+	}
+
+	@Override
+	public ModelPart root() {
+		return body;
+	}
+
+	private void animate(YellowWispEntity entity, AnimationDefinition animation, float time, float scale)
+	{
+		KeyframeAnimations.animate(this, animation, (long) (time * 1000L), scale, new Vector3f(0,0,0));
+	}
+
+	@Override
+	public void setupAnim(YellowWispEntity yellowWispEntity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
+		this.root().getAllParts().forEach(ModelPart::resetPose);
+		this.root().xRot = 0;
+		float animTime = animationProgress / 20;
+		animate(yellowWispEntity, EntityAnimations.YELLOW_WISP_MAIN, animTime, 1f);
+	}
+}
