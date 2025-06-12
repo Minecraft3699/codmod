5d47eb1ef319ef6cf5044cf8b730f9d33bc57275
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2025-06-12 16:05:27 -0700

    E

--- src/main/java/com/mc3699/codmod/item/CodRodItem.java
+++ src/main/java/com/mc3699/codmod/item/CodRodItem.java
@@ -1,17 +1,22 @@
 package com.mc3699.codmod.item;
 
+import com.mc3699.codmod.registry.CodDamageTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Cod;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.MaceItem;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
@@ -24,32 +29,69 @@ public class CodRodItem extends MaceItem {
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        if(entity.level() instanceof ServerLevel serverLevel)
-        {
+        if (entity.level() instanceof ServerLevel serverLevel) {
             Vec3 codSpawnPos = entity.getPosition(0);
 
-            if(entity instanceof ServerPlayer hitPlayer)
-            {
-                entity.kill();
+            DamageSource dam = new DamageSource(player.level()
+                    .registryAccess()
+                    .lookupOrThrow(Registries.DAMAGE_TYPE)
+                    .getOrThrow(CodDamageTypes.COD_ROD));
+
+            if (entity instanceof ServerPlayer hitPlayer) {
+                entity.hurt(dam, 1000f);
                 //hitPlayer.connection.disconnect(Component.literal("Whatever reason you want blah blah blah"));
             } else {
                 entity.remove(Entity.RemovalReason.DISCARDED);
             }
 
 
-            serverLevel.playSound(null, BlockPos.containing(codSpawnPos), SoundEvents.DRAGON_FIREBALL_EXPLODE, SoundSource.PLAYERS);
+            serverLevel.playSound(
+                    null,
+                    BlockPos.containing(codSpawnPos),
+                    SoundEvents.DRAGON_FIREBALL_EXPLODE,
+                    SoundSource.PLAYERS
+            );
 
-            for(int i = 0; i < 8; i++)
-            {
+            for (int i = 0; i < 8; i++) {
                 Entity cod = new Cod(EntityType.COD, serverLevel);
                 cod.setPos(codSpawnPos);
-                int randomVelX = serverLevel.random.nextInt(-4,4);
-                int randomVelZ = serverLevel.random.nextInt(-4,4);
-                int randomHeight = serverLevel.random.nextInt(8,24);
-                serverLevel.sendParticles(ParticleTypes.EXPLOSION, codSpawnPos.x, codSpawnPos.y, codSpawnPos.z, 2, 0,1,0, randomVelX);
-                serverLevel.sendParticles(ParticleTypes.FLAME, codSpawnPos.x, codSpawnPos.y, codSpawnPos.z, 32, 0,1,0,randomVelX * 0.01);
-                serverLevel.sendParticles(ParticleTypes.CAMPFIRE_COSY_SMOKE, codSpawnPos.x, codSpawnPos.y, codSpawnPos.z, 4, 0,1,0, 0.01);
-                cod.setDeltaMovement(randomVelX*0.1,randomHeight*0.05,randomVelZ*0.1);
+                int randomVelX = serverLevel.random.nextInt(-4, 4);
+                int randomVelZ = serverLevel.random.nextInt(-4, 4);
+                int randomHeight = serverLevel.random.nextInt(8, 24);
+                serverLevel.sendParticles(
+                        ParticleTypes.EXPLOSION,
+                        codSpawnPos.x,
+                        codSpawnPos.y,
+                        codSpawnPos.z,
+                        2,
+                        0,
+                        1,
+                        0,
+                        randomVelX
+                );
+                serverLevel.sendParticles(
+                        ParticleTypes.FLAME,
+                        codSpawnPos.x,
+                        codSpawnPos.y,
+                        codSpawnPos.z,
+                        32,
+                        0,
+                        1,
+                        0,
+                        randomVelX * 0.01
+                );
+                serverLevel.sendParticles(
+                        ParticleTypes.CAMPFIRE_COSY_SMOKE,
+                        codSpawnPos.x,
+                        codSpawnPos.y,
+                        codSpawnPos.z,
+                        4,
+                        0,
+                        1,
+                        0,
+                        0.01
+                );
+                cod.setDeltaMovement(randomVelX * 0.1, randomHeight * 0.05, randomVelZ * 0.1);
                 serverLevel.addFreshEntity(cod);
             }
         }
@@ -58,8 +100,7 @@ public class CodRodItem extends MaceItem {
 
     @Override
     public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miningEntity) {
-        if(miningEntity instanceof Player player && player.isCreative())
-        {
+        if (miningEntity instanceof Player player && player.isCreative()) {
             return false;
         } else {
             return true;
