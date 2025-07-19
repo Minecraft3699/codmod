f3585aa4b1c54c4844080115c282319085102fa2
Minecraft3699 <dylandirkes256@outlook.com>
2025-07-19 17:05:00 -0500

    poolrooms gen.stuff

--- src/main/java/com/mc3699/codmod/backrooms_dimension/levels/Poolrooms.java
+++ src/main/java/com/mc3699/codmod/backrooms_dimension/levels/Poolrooms.java
@@ -5,11 +5,15 @@ import com.mc3699.codmod.backrooms_dimension.BackroomsLevel;
 import com.mc3699.codmod.registry.CodBlocks;
 import foundry.veil.api.client.render.light.Light;
 import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LightBlock;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.synth.PerlinNoise;
+
+import java.util.List;
 
 public class Poolrooms extends BackroomsLevel {
 
@@ -23,17 +27,32 @@ public class Poolrooms extends BackroomsLevel {
         return 22;
     }
 
+    public static final PerlinNoise FilledNoise = PerlinNoise.create(RandomSource.create(), List.of(1,5,9,9,38,1,2,43));
+    public static final PerlinNoise LargeWallNoise = PerlinNoise.create(RandomSource.create(), List.of(33,1,38,29,19,21));
+    public static final PerlinNoise PlatformNoise = PerlinNoise.create(RandomSource.create(), List.of(32,69,420,69,69,128,69,420));
+
     @Override
     public void generate(WorldGenRegion worldGenRegion, StructureManager structureManager, RandomState randomState, ChunkAccess chunkAccess) {
         BRGenUtil.fillLayer(chunkAccess, getStartLevel(), CodBlocks.CERAMIC_TILE.get().defaultBlockState());
 
         BRGenUtil.fillLayer(chunkAccess, getStartLevel()+3, Blocks.LIGHT.defaultBlockState().setValue(LightBlock.LEVEL, 15));
         BRGenUtil.fillLayer(chunkAccess, getEndLevel()-3, Blocks.LIGHT.defaultBlockState().setValue(LightBlock.LEVEL, 15));
-
-
-
         BRGenUtil.fillLayer(chunkAccess, getStartLevel()+1, Blocks.WATER.defaultBlockState());
         BRGenUtil.fillLayer(chunkAccess, getEndLevel(), CodBlocks.CERAMIC_TILE.getDefaultState());
+
+        if(BRGenUtil.isChunkInNoise(chunkAccess, FilledNoise, 0.05))
+        {
+            BRGenUtil.fillArea(chunkAccess, 0, getStartLevel(), 0, 15, getEndLevel(), 15, CodBlocks.CERAMIC_TILE.getDefaultState());
+        } else if (BRGenUtil.isChunkInNoise(chunkAccess, LargeWallNoise, 0.06)) {
+            BRGenUtil.generateBasicWalls(chunkAccess, getStartLevel(), getEndLevel(), CodBlocks.CERAMIC_TILE.getDefaultState());
+        }
+
+        if(BRGenUtil.isChunkInNoise(chunkAccess, PlatformNoise, 0.02))
+        {
+
+        }
+
+
     }
 
 
