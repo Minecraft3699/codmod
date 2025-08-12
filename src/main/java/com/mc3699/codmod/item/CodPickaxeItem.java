5ff66bcbaea9faca6f84e1f1cba1ae003e6338d3
Lumi <something@noreply>
2025-08-12 11:18:05 +0200

    Cod

---
+++ src/main/java/com/mc3699/codmod/item/CodPickaxeItem.java
@@ -0,0 +1,13 @@
+package com.mc3699.codmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Tiers;
+
+public class CodPickaxeItem extends PickaxeItem {
+    public CodPickaxeItem(Properties properties) {
+        super(Tiers.NETHERITE,properties);
+    }
+
+
+}
